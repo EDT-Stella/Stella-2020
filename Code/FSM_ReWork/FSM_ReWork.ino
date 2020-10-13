efcc39b059e0e25b2652bb206de03b04b7b24121
deven5809 <47309349+deven5809@users.noreply.github.com>
2020-10-13 18:31:34 -0500

    Update FSM_ReWork.ino

--- Code/FSM_ReWork/FSM_ReWork.ino
+++ Code/FSM_ReWork/FSM_ReWork.ino
@@ -1,7 +1,7 @@
 
 bool RotateAuger = false;
 bool ColorSense = false;
-bool RotareBarrel = false;
+bool RotateBarrel = false;
 
 enum colorList{ 
   Red, 
@@ -15,51 +15,59 @@ enum colorList{
   
 }storeColor, dropColor;
 
- void setSortColor(){
-    if(storeColor == Red) {
-
-      dropColor = Purple;
-      
-    }
-    else if (storeColor == Orange){
-
-      dropColor = Yellow;
-      
-    }
-    else if (storeColor == Yellow){
-
-      dropColor = Orange;
-  
-      
-    }
-    else if (storeColor == Green){
-
-        dropColor = LightBlue;
-    }
-    else if (storeColor == LightBlue){
-
-      dropColor = Green;
-    }
-    else if (storeColor == Purple) 
-    {
-
-      dropColor = Red;
-    }
+void setSortColor(){
+  if(storeColor == Red) {
+    dropColor = Purple; 
+  }
+  else if (storeColor == Orange){
+    dropColor = Yellow; 
+  }
+  else if (storeColor == Yellow){
+    dropColor = Orange;
+  }
+  else if (storeColor == Green){
+    dropColor = LightBlue;
+  }
+  else if (storeColor == LightBlue){
+    dropColor = Green;
+  }
+  else if (storeColor == Purple) 
+  {
+    dropColor = Red;
+  }
  }
+
+ bool rotateAugerUp() {
+  if (!ColorSense) {
+    if (!RotateBarrel) {
+      //Rotate Auger
+    }
+  }
+  return true;
+}
+
+bool rotateAugerDown() {
+  if (!ColorSense) {
+    if (!RotateBarrel) {
+      //Rotate Auger
+    }
+  }
+  return true;
+}
+
+bool getColor() {
+  if (!RotateAuger) {
+    if (!RotateBarrel) {
+      //Call Color sensor
+    }
+  }
+}
 
 
 void setup() {
   storeColor = Red ;
   dropColor = Purple;
 
- 
-
-  
-
-    
-  
-
-  
 }
 
 void loop() {
