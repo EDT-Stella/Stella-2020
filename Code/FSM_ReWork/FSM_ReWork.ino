b84a2fa295cecae5af62e84b0d2f5204834daba9
ShaneJoy <72474754+ShaneJoy@users.noreply.github.com>
2020-10-13 18:13:18 -0500

    Update FSM_ReWork.ino

--- Code/FSM_ReWork/FSM_ReWork.ino
+++ Code/FSM_ReWork/FSM_ReWork.ino
@@ -1,6 +1,65 @@
-void setup() {
-  // put your setup code here, to run once:
 
+bool RotateAuger = false;
+bool ColorSense = false;
+bool RotareBarrel = false;
+
+enum colorList{ 
+  Red, 
+  Orange,
+  Yellow,
+  Green,
+  LightBlue,
+  DarkBlue,
+  Purple,
+  Pink
+  
+}storeColor, dropColor;
+
+ void setSortColor(){
+    if(storeColor == Red) {
+
+      dropColor = Purple;
+      
+    }
+    else if (storeColor == Orange){
+
+      dropColor = Yellow;
+      
+    }
+    else if (storeColor == Yellow){
+
+      dropColor = Orange;
+  
+      
+    }
+    else if (storeColor == Green){
+
+        dropColor = LightBlue;
+    }
+    else if (storeColor == LightBlue){
+
+      dropColor = Green;
+    }
+    else if (storeColor == Purple) 
+    {
+
+      dropColor = Red;
+    }
+ }
+
+
+void setup() {
+  storeColor = Red ;
+  dropColor = Purple;
+
+ 
+
+  
+
+    
+  
+
+  
 }
 
 void loop() {
