2cd3b2911f27acaccc5cd135960b920e7dbd4b90
adenprince <45126168+adenprince@users.noreply.github.com>
2020-10-27 16:53:02 -0500

    Added mapping RGB values to a color
    The rotateBarrel function by Dan has also been added.

--- Code/FSM_ReWork/FSM_ReWork.ino
+++ Code/FSM_ReWork/FSM_ReWork.ino
@@ -1,4 +1,9 @@
 //Stella Receiver
+// Authors:
+//  Dan Even
+//  Shane J
+//  Aden Prince
+
 
 #include "Adafruit_TCS34725.h"
 
@@ -9,8 +14,9 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS3472
 volatile bool RotateAuger = false;
 volatile bool ColorSense = false;
 volatile bool RotateBarrel = false;
+volatile bool ExtendingBarrel = false;
 
-enum colorList{ 
+enum color { 
   Red, 
   Orange,
   Yellow,
@@ -19,26 +25,35 @@ enum colorList{
   DarkBlue,
   Purple,
   Pink
-}storeColor, dropColor;
+} storeColor, dropColor;
 
-void setSortColor(){
-  if(storeColor == Red) {
+// RGB values for each color
+int colorMap[8][3] = {{145, 53, 45},   // Red
+                      {147, 60, 34},   // Orange
+                      {125, 128, 45},  // Yellow
+                      {76, 113, 50},   // Green
+                      {39, 97, 113},   // Light Blue
+                      {28, 78, 136},   // Dark Blue
+                      {72, 67, 105},   // Purple
+                      {104, 54, 81}};  // Pink
+
+void setDropColor() {
+  if (storeColor == Red) {
     dropColor = Purple; 
   }
-  else if (storeColor == Orange){
+  else if (storeColor == Orange) {
     dropColor = Yellow; 
   }
-  else if (storeColor == Yellow){
+  else if (storeColor == Yellow) {
     dropColor = Orange;
   }
-  else if (storeColor == Green){
+  else if (storeColor == Green) {
     dropColor = LightBlue;
   }
-  else if (storeColor == LightBlue){
+  else if (storeColor == LightBlue) {
     dropColor = Green;
   }
-  else if (storeColor == Purple) 
-  {
+  else if (storeColor == Purple) {
     dropColor = Red;
   }
 }
@@ -61,19 +76,65 @@ bool rotateAugerDown() {
   return true;
 }
 
+// Use the sensor to get the color of the ball to be sorted
 bool getColor() {
+  ColorSense = true;
   if (!RotateAuger) {
     if (!RotateBarrel) {
-      //Call Color sensor
+      // Call Color sensor
       float red, green, blue;
-      tcs.setInterrupt(false);  //Turn on LED
+      tcs.setInterrupt(false); // Turn on LED
       delay(60);
-      tcs.setInterrupt(true);  //Turn off LED
+      tcs.setInterrupt(true);  // Turn off LED
       tcs.getRGB(&red, &green, &blue); // Read color values
+      storeColor = RGBToColor(red, green, blue);
+      setDropColor();
     }
   }
 }
 
+// Check if the first parameter is within +/- 10 of the second parameter
+// Parameters:
+// int val1, val2 - values to compare
+// Return:
+// bool of of whether the first value is within +/- 10 of the second value
+bool within10(int val1, int val2) {
+  return (val1 <= val2 + 10 && val1 >= val2 - 10);
+}
+
+// Get a color based on passed RGB values using the color map
+// Parameters:
+// float red, green, blue - color values from the color sensor
+// Return:
+// color corresponding to the passed RGB values
+color RGBToColor(float red, float green, float blue) {
+  // Iterate through color map
+  for (int i = 0; i < 8; ++i) {
+    // Check if the passed RGB values are close to the current color
+    if (within10(red, colorMap[i][0]) &&
+        within10(green, colorMap[i][1]) &&
+        within10(blue, colorMap[i][2])) {
+      // Return current color
+      return (color)i;
+    }
+  }
+
+  // FIX: Return Red (0) when color not detected
+  return Red;
+}
+
+// Rotates the barrel to dispense/sort the correct color
+// Parameters:
+// color c - the color to be dispensed/sorted to
+// bool sort - if TRUE: move to appropriate color for sorting
+//                FALSE: move to appropriate color for dispensing
+bool rotateBarrel(color c, bool sort) {
+    if (!RotateAuger) {
+      if (!ColorSense) {
+        
+      }
+    }
+}
 
 void setup() {
   //-----------------------------------------
@@ -87,7 +148,7 @@ void setup() {
   }
   //-----------------------------------------
   
-  storeColor = Red ;
+  storeColor = Red;
   dropColor = Purple;
 
 }
