e2b7435b0196f0be267acd06aaa2e11a306b855d
NirajSalunkhe <47925956+NirajSalunkhe@users.noreply.github.com>
2020-11-10 18:33:42 -0600

    Update StellaRX_Scheduler.ino

--- Code/StellaRX_Scheduler/StellaRX_Scheduler.ino
+++ Code/StellaRX_Scheduler/StellaRX_Scheduler.ino
@@ -30,6 +30,9 @@ volatile byte BARREL_IN_POSITION = false;
 //Radio interrupt Flag
 volatile byte MSSG = false; 
 
+// Barrel Stepper Motor interrupt flag.
+volatile byte ROTATE_BARREL = false;
+
 //Radio new data flag
 bool newData = false;
 //===============================================
@@ -287,39 +290,57 @@ bool DropBall::canRun(uint32_t now) {
 //==================================================================
 
 //===================BarrelRotateStepper============================
-//class BarrelRotateStepper : public TimedTask
-//{
-//  public:
-//  BarrelRotateStepper(short _currentColor, short _targetColor,  Debugger *_ptrDebugger);
-//  virtual void run(uint32_t schTime);
-//
-//  private:
-//  short currentColor; // The current index the motor is at
-//  short targetColor; // The target index for the motor to move to
-//};
-//
-//BarrelRotateStepper::BarrelRotateStepper(short _currentColor, short _targetColor, Debugger *_ptrDebugger){
-//  TimedTask(millis());
-//  currentColor(_currentColor);
-//  targetColor(_targetColor);
-//}
-//
-//void BarrelRotateStepper::run(uint32_t schTime){
-//  short distance = targetColor - currentColor;
-//
-//  // Moves the opposite direction if the distance is greater than 3
-//  if (distance > 3) {
-//    distance -= 6;
-//  }
-//  else if (distance < -3) {
-//    distance += 6;
-//  }
-//
-//  int targetPos = tic.getCurrentPosition() + (distance * 33.0);
-//  tic.setTargetPosition(targetPos);
-//  
-//  waitForPosition(targetPos);
-//}
+class BarrelRotateStepper : public TriggeredTask
+{
+  public:
+  BarrelRotateStepper(uint32_t _pin1, uint32_t _pin2, short _currentColor, short _targetColor);
+  void waitForPosition(int32_t targetPosition);
+  virtual void run(uint32_t now);
+  virtual bool canRun(uint32_t now);
+
+  private:
+  uint32_t pin1;
+  uint32_t pin2;
+  short currentColor; // The current index the motor is at
+  short targetColor; // The target index for the motor to move to
+  
+};
+
+BarrelRotateStepper::BarrelRotateStepper(uint32_t _pin1, uint32_t _pin2, short _currentColor, short _targetColor):TriggeredTask(), pin1(_pin1), pin2(_pin2) {
+  SoftwareSerial ticSerial(pin1, pin2);
+  TicSerial tic(ticSerial);
+  currentColor = _currentColor;
+  targetColor = _targetColor;
+}
+
+void BarrelRotateStepper::run(uint32_t now){
+  short distance = targetColor - currentColor;
+
+  // Moves the opposite direction if the distance is greater than 3
+  if (distance > 3) {
+    distance -= 6;
+  }
+  else if (distance < -3) {
+    distance += 6;
+  }
+
+  int32_t targetPos = tic.getCurrentPosition() + (distance * 33.0);
+  tic.setTargetPosition(targetPos);
+  
+  waitForPosition(targetPos);
+
+  currentColor = targetColor;
+}
+
+bool BarrelRotateStepper::canRun(uint32_t now) {
+  return ROTATE_BARREL;
+}
+
+void BarrelRotateStepper::waitForPosition(int32_t targetPosition) {
+  do {
+    tic.resetCommandTimeout();
+  } while (tic.getCurrentPosition() != targetPosition);
+}
 //==================================================================
 
 //==================================================================
