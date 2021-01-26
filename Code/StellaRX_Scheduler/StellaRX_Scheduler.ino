a9d55869fa60378aa9498518642910ba2d723ecd
deven2UIC <70229259+deven2UIC@users.noreply.github.com>
2021-01-25 19:52:26 -0600

    Buttons mapped to keyboard

--- Code/StellaRX_Scheduler/StellaRX_Scheduler.ino
+++ Code/StellaRX_Scheduler/StellaRX_Scheduler.ino
@@ -92,7 +92,7 @@ struct dataReceived { // Data from the controller
   //Button 4 Pick/Drop: Unused
   //Button JS Pickup: Rotate Auger Toggle
   //Rocker: Switch Between Pickup and Drop States
-  bool button1, button2, button3, button4, rocker, jsButton;
+  bool button1, button2, button3, button4, rocker1, rocker2, jsButton;
 
   byte ballColor;
 };
@@ -116,90 +116,6 @@ void dataReceived_Interrupt() {
   MSSG = true;
   //getData();
 }
-
-///*****************************************************************************************
-//*  Class:    Debugger
-//* Task Type:  Task (always runs)
-//* Purpose:  This expands on Alan Burlison's original example code which demonstrates
-//*       a task that reads from the serial port and echoes to the Serial Monitor.
-//*       I've expanded it so that other classes use a pointer to the debugger object
-//*       to output simple debug messages while this example executes.
-//*       Classes that use the debugger object are passed a reference to &debugger
-//*       in their respective constructors.
-//*
-//*       For example: Blinker(uint8_t _pin, uint32_t _rate, Debugger *_ptrDebugger);
-//*
-//*       To output debug information use: ptrDebugger->debugWrite("debug info");
-//*
-//* Notes:    Yeah, I lazily used the String() function in this demonstration. Suedfbvbvfbfvvvvvvvb  me.            
-//******************************************************************************************/
-//
-//// ***
-//// *** Define the Debugger Class as type Task
-//// ***
-//class Debugger : public Task
-//{
-//public:
-//  Debugger();
-//  void debugWrite(String debugMsg); //Used for simple debugging of other tasks
-//  virtual void run(uint32_t now);   //Override the run() method
-//  virtual bool canRun(uint32_t now);
-//};
-//
-//// ***
-//// *** Debugger Constructor
-//// ***
-//Debugger::Debugger()
-//  : Task()
-//  {
-//    Serial.begin(57600);
-//  }
-//
-//// ***
-//// *** Debugger::canRun() <--checked by TaskScheduler
-//// ***
-//bool Debugger::canRun(uint32_t now)
-//{
-//  return Serial.available() > 0;
-//}
-//
-//// ***
-//// *** Debugger::run() <--executed by TaskScheduler as a result of canRun() returning true.
-//// ***
-//void Debugger::run(uint32_t now)
-//{
-//  uint16_t byteCount = 0;
-//  
-//  Serial.println("-----------------");
-//  Serial.println("Input Received...");
-//  Serial.println("-----------------");
-//  while (Serial.available() > 0) {
-//    int byte = Serial.read();
-//    Serial.print("'") ;
-//    Serial.print(char(byte));
-//    Serial.print("' = ");
-//    Serial.print(byte, DEC);
-//    Serial.println(" ");
-//    if (byte == '\r') {
-//      Serial.print('\n', DEC);
-//    }
-//    
-//    byteCount++;
-//  }
-//  
-//  Serial.println("-----------------");
-//  Serial.print("Bytes Received: "); Serial.println(String(byteCount));
-//  Serial.println("-----------------");
-//  
-//}
-//
-//// ***
-//// *** Debugger::debugWrite() <--provides basic debug info from other tasks
-//// ***
-//void Debugger::debugWrite(String debugMsg)
-//{
-//  Serial.println(debugMsg);
-//}
 
 ////======================Example Task==============================
 //class Blinker : public TimedTask
@@ -259,9 +175,9 @@ public:
   virtual void run(uint32_t schTime);
 
   private:
-
-  
 };
+
+
 //==================================================================
 class AugerMoveActuator : public TimedTask
 {
@@ -311,7 +227,6 @@ DropBall::DropBall(uint8_t _pin) : TriggeredTask(), pin(_pin), dropCondition(fal
   dropDoor.attach(pin);
   dropDoor.write(0);
   
-   
 }
 
 void DropBall::run(uint32_t now) {
@@ -548,7 +463,29 @@ KeyboardInput::KeyboardInput(uint8_t _pin) {
 void KeyboardInput::run(uint32_t now)
 {
   String input = Serial.readString();
-  Serial.println(input);
+
+  if (input == "1") {
+    data.button1 = true;
+  }
+  else if (input == "2") {
+    data.button2 = true;
+  }
+  else if (input == "3") {
+    data.button3 = true;
+  }
+  else if (input == "4") {
+    data.button4 = true;
+  }
+  else if (input == "r") {
+    data.rocker1 = !data.rocker1;
+  }
+  else if (input == "t") {
+    data.rocker2 = !data.rocker2;
+  }
+  else if (input == "j") {
+    data.jsButton = true;
+  }
+  Serial.print(input);
 }
 
 bool KeyboardInput::canRun(uint32_t now)
